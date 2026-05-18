742b99f1555c5a368b3144ee86594a043531b376
mouse1999 <kufreedward26@gmail.com>
2026-05-18 14:04:48 +0100

    feat: add circuit breaker for ai

---
+++ worker-java/src/main/java/com/vulnwatch/worker/circuitbreaker/OpenAiCircuitBreaker.java
@@ -0,0 +1,8 @@
+package com.vulnwatch.worker.circuitbreaker;
+
+import com.vulnwatch.worker.models.AggregatedScanData;
+
+public class OpenAiCircuitBreaker {
+    public void enrichWithProtection(AggregatedScanData aggregatedScanData) {
+    }
+}
