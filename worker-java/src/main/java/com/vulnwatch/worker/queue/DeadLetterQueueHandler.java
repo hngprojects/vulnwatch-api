458756b6f94d5247ca87a00c2a394b788378e1de
mouse1999 <kufreedward26@gmail.com>
2026-05-18 14:05:30 +0100

    feat: add empty dead letter queue handler for failed event

---
+++ worker-java/src/main/java/com/vulnwatch/worker/queue/DeadLetterQueueHandler.java
@@ -0,0 +1,8 @@
+package com.vulnwatch.worker.queue;
+
+import com.vulnwatch.worker.event.SurfaceResultEvent;
+
+public class DeadLetterQueueHandler {
+    public void moveToDeadLetter(SurfaceResultEvent event) {
+    }
+}
